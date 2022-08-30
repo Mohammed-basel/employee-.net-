ad8bc903beba0d6065ca61f06ea85f6b07ff1119
mohammed basel <User@USER-PC>
2022-08-30 14:22:07 +0300

    first

---
+++ emplyee api/Program.cs
@@ -0,0 +1,34 @@
+using emplyee_api.services;
+using Microsoft.AspNetCore.Mvc;
+using static emplyee_api.services.IEmplyee;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options =>
+{
+    options.Filters.Add(new RequireHttpsAttribute());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
